dc565d780f553573bca9429cb837accd2e643948
Soggymender <41552995+Soggymender@users.noreply.github.com>
2022-12-01 18:36:15 -0500

    day 1 part 1

---
+++ day1/part1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <ios>
+#include <fstream>
+
+int main()
+{
+  std::string filename = "input.txt";
+
+  std::ostringstream dosString(std::ios::out | std::ios::binary);
+  std::ifstream inFile(filename.c_str());
+
+  int total = 0;
+  int max = 0;
+
+  std::string line;
+  while(std::getline(inFile, line)) {
+
+    if (line.length() == 0) {
+
+      if (total > max) {
+        max = total;
+      }
+
+      total = 0;
+      continue;
+    }
+
+    int num = stoi(line);
+
+    total += num;
+  }
+
+  std::cout << max;
+
+  return 0;
+}
